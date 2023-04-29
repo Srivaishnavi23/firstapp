ac77b519f94cb1ac6f379e6481628f587a5138e6
Srivaishnavi23 <83265272+Srivaishnavi23@users.noreply.github.com>
2023-04-29 12:31:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.write('My name is ChatGPT!');
+  res.write('My name is vaishnavi!');
   res.end();
 });
 
